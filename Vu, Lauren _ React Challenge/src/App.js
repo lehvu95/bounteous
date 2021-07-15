a5c00eae21499adc4d41f77ec32026da9d44df28
lehvu95 <lehvu95@gmail.com>
2021-07-15 12:38:53 -0500

    to do list

--- Vu, Lauren _ React Challenge/src/App.js
+++ Vu, Lauren _ React Challenge/src/App.js
@@ -2,6 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// create mock data to use based off of readme
+// create getTVShowNames function which filters by input string
+// -> convert name to lowercase 
+// -> convert input string to lowercase
+// -> search lowercase name for input string
+// -> use filter function
+// -> sort array in ascending order by name
+// -> print list in console
+// create search results component
+// -> image.medium for image
+// -> filter results by shows that have an image
+// -> see readme for sample output
+// use axios to perform fetch
+// create form for search input
+// -> check codesandbox for solution with state
+// -> check github for solution with reducer
+
 function App() {
   return (
     <div className="App">
